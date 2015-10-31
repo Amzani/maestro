b1f3588411e98413273872c87c9ea123b7bb8d79
Matthieu Hauglustaine <matt.hauglustaine@gmail.com>
2015-10-31 14:21:21 +0100

    fsutil: add the clone method to clone a directory.

--- fsutils.py
+++ fsutils.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 from contextlib import contextmanager
 
@@ -17,3 +18,23 @@ def pushd(path=os.getenv('HOME', None), create=True):
     os.chdir(path)
     yield
     os.chdir(old_path)
+
+def clone(src, dst):
+    """Recursively copy the files and directories of the src dir into dst.
+    Note that symlinks are ignored.
+    """
+    names = os.listdir(src)
+
+    if not os.path.exists(dst):
+        os.makedirs(dst)
+    elif not os.path.isdir(dst):
+        raise ValueError('\'dst\' is not a directory')
+
+    for name in names:
+        srcname = os.path.join(src, name)
+        dstname = os.path.join(dst, name)
+
+        if os.path.isdir(srcname):
+            clone(srcname, dstname)
+        elif os.path.isfile(srcname) and not os.path.islink(srcname):
+            shutil.copy2(srcname, dstname)
