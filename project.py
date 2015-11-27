1d4681d1ee10f923c2b7dd51601480b238de80b0
Matthieu Hauglustaine <matthieu.hauglustaine@dailymotion.com>
2015-11-27 14:43:22 +0100

    project: use working directory to look for git project.

--- project.py
+++ project.py
@@ -2,8 +2,7 @@ import os
 
 import git
 
-_repo = git.Repo(os.path.dirname(__file__),
-                 search_parent_directories=True)
+_repo = git.Repo(os.getcwd(), search_parent_directories=True)
 
 build_dir = os.path.join(_repo.working_dir, 'build')
 images_dir = os.path.join(_repo.working_dir, 'images')
