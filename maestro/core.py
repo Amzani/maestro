592e6acf6c70faf1ff83c8218978f75af9629e1c
Matthieu Hauglustaine <matthieu.hauglustaine@dailymotion.com>
2015-11-30 14:24:07 +0100

    core: use the cli and context modules.

--- maestro/core.py
+++ maestro/core.py
@@ -1,33 +1,17 @@
 import argparse
 import textwrap
 
+from . import cli
 from .builders import Builder
+from .context import ExecContext
 
-def parse_args():
-    parser = argparse.ArgumentParser(
-        prog='maestro',
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        description='The best SOA build tool.',
-        epilog=textwrap.dedent('''\
-        examples:
-            maestro image build my-image
-            maestro service build my-service-foo
-            maestro service test my-service-foo
-        '''))
-    parser.add_argument('builder', type=str,
-                        choices=['image', 'service'],
-                        help='type of builder')
-    parser.add_argument('action', type=str,
-                        help='action the builder must perform')
-    parser.add_argument('target', type=str,
-                        help='target on which the builder must perform the action')
-    return parser.parse_args()
 
 def main():
-    args = parse_args()
+    ctx = ExecContext()
+    cli.parser.parse_args(namespace=ctx)
 
-    builder = Builder.from_name(args.builder)
-    builder.call(args.action, args.target)
+    builder = Builder.from_name(ctx.builder, exec_ctx=ctx)
+    builder.call(ctx.action, ctx.target)
 
 if __name__ == '__main__':
     main()
